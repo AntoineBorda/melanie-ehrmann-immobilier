fc48245591adb8575913648a2c4d8fc610854399
AntoineBorda <pro@antoineborda.com>
2024-07-19 10:45:54 +0200

    [CONFIG] Add & configure VichUploaderBundle for file uploading

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -4,8 +4,11 @@ namespace App\Entity;
 
 use App\Repository\ImageRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: ImageRepository::class)]
+#[Vich\Uploadable]
 class Image
 {
     #[ORM\Id]
@@ -13,20 +16,20 @@ class Image
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Vich\UploadableField(mapping: 'offers', fileNameProperty: 'imageName', size: 'imageSize', mimeType: 'mimeType')]
+    private ?File $imageFile = null;
+
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
 
     #[ORM\Column(nullable: true)]
-    private ?\DateTimeImmutable $updatedAt = null;
+    private ?string $imageName = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $fileName = null;
+    #[ORM\Column(nullable: true)]
+    private ?int $imageSize = null;
 
     #[ORM\Column(length: 255)]
     private ?string $mimeType = null;
-
-    #[ORM\Column]
-    private ?int $size = null;
 
     public function getId(): ?int
     {
@@ -45,28 +48,29 @@ class Image
         return $this;
     }
 
-    public function getUpdatedAt(): ?\DateTimeImmutable
+    public function getImageFile(): ?File
     {
-        return $this->updatedAt;
+        return $this->imageFile;
     }
 
-    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static
+    public function getImageName(): ?string
     {
-        $this->updatedAt = $updatedAt;
-
-        return $this;
+        return $this->imageName;
     }
 
-    public function getFileName(): ?string
+    public function setImageName(?string $imageName): void
     {
-        return $this->fileName;
+        $this->imageName = $imageName;
     }
 
-    public function setFileName(string $fileName): static
+    public function getImageSize(): ?int
     {
-        $this->fileName = $fileName;
+        return $this->imageSize;
+    }
 
-        return $this;
+    public function setImageSize(?int $imageSize): void
+    {
+        $this->imageSize = $imageSize;
     }
 
     public function getMimeType(): ?string
@@ -74,22 +78,8 @@ class Image
         return $this->mimeType;
     }
 
-    public function setMimeType(string $mimeType): static
+    public function setMimeType(?string $mimeType): void
     {
         $this->mimeType = $mimeType;
-
-        return $this;
-    }
-
-    public function getSize(): ?int
-    {
-        return $this->size;
-    }
-
-    public function setSize(int $size): static
-    {
-        $this->size = $size;
-
-        return $this;
     }
 }
